2b9d22f51da5a4a7eb34fd124a2079039286191a
Tantatorn <timefear007@gmail.com>
2020-02-08 15:32:21 +0700

    change from json to db

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
 let express = require('express')
 let fs = require('fs')
-let app = express()
 let cors = require('cors')
 let nanoid = require('nanoid')
 let path = require('path')
+
+const { Client } = require('pg')
+const client = new Client({
+    host: 'ec2-3-234-109-123.compute-1.amazonaws.com',
+    user: 'avrrhvttuasjer',
+    password: '4e85ad72f51e0239d08abb7ae72018b19f609a0015454f6af2c775c4facc3462',
+    database: 'd4naqt4ilruq0e',
+    ssl: true
+})
+
+client.connect()
+
+let app = express()
+
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-let PORT = process.env.PORT || 1234
-const filePath = path.join(__dirname, 'data.json')
-let exist = fs.existsSync(filePath)
-let app_data = {
-    posts: [],
-    user: []
-}
 
-if (exist) {
-    try {
-        app_data = JSON.parse(fs.readFileSync(filePath, { encoding: 'utf-8' }))
-    } catch (err) {
-        app_data = app_data = {
-            posts: [],
-            user: []
-        }
-    }
-} else {
-    fs.openSync(filePath, 'w', (err) => {
-        throw err
-    })
-}
+let PORT = process.env.PORT || 3000
 
 app.post('/api/user/login', (req, res) => {
     let { username, password } = req.body
@@ -36,73 +29,82 @@ app.post('/api/user/login', (req, res) => {
         res.status(400).send('Check username or password')
         return
     }
-    let index = app_data.user.findIndex(v => v.username === username && v.password === password)
-    if (index === -1) {
-        res.status(401).send('not found user')
-    } else {
-        res.status(200).send('success')
-    }
+
+    client.query('SELECT * FROM account WHERE username=$1', [username], (err, data) => {
+        if (err) {
+            res.json({ 'message': 'user not found' })
+        }
+        else if (data.rows[0] == undefined) {
+            res.json({ 'message': 'user not found' })
+        }
+        else {
+            if (data.rows[0]['password'] != password) {
+                res.json({ 'message': 'password not matched' })
+            }
+            else {
+                res.json({ 'message': 'logged in' })
+            }
+        }
+    })
+
 })
 
 app.post('/api/user', (req, res) => {
     let { username, password } = req.body
-    let index = app_data.user.findIndex(v => v.username === username)
-    if (index !== -1) {
-        res.status(401).send('username already taken')
-    } else {
-        app_data.user.push({
-            username,
-            password
+    client.query('INSERT INTO account(username,password) VALUES ($1,$2)', [username, password],
+        (err) => {
+            if (err) {
+                console.log(err.stack)
+            }
         })
-        fs.writeFileSync(filePath, JSON.stringify(app_data))
-        res.status(200).send('success')
-    }
+    res.status(201).send("User created")
 })
 
-app.get('/api/posts', (req, res) => {
-    res.json(app_data.posts.map(v => {
-        return {
-            id: v.id,
-            name: v.name,
-            topic: v.topic
+app.get('/api/posts', async (req, res) => {
+    client.query('SELECT * FROM post', (err, data) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            res.json(data.rows)
         }
     })
-    )
 })
 
 app.post('/api/posts', (req, res) => {
     let { name, topic, content } = req.body
-    console.log(req.body)
-    let tempId = nanoid(6)
-    app_data.posts.push({
-        id: tempId,
-        name,
-        topic,
-        content,
-        comment: []
-    })
-    fs.writeFileSync(filePath, JSON.stringify(app_data))
-    res.status(201).send(tempId)
+    client.query('INSERT INTO post(username,topic,content) VALUES ($1,$2,$3)', [name, topic, content],
+        (err) => {
+            if (err) {
+                console.log(err.stack)
+            }
+        })
+    res.status(201).send("Post created")
 })
 
 app.get('/api/posts/:id', (req, res) => {
     let { id } = req.params
-    let target = app_data.posts.filter(v => v.id === id)
-    res.json(target.pop())
+    client.query('SELECT * FROM post WHERE id=$1', [id], (err, data) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            res.json(data.rows[0])
+        }
+    })
 })
 
 app.post('/api/posts/:id/reply', (req, res) => {
     let { id } = req.params
     let { name, reply } = req.body
-    let targetIndex = app_data.posts.findIndex(v => v.id === id)
-    app_data.posts[targetIndex].comment.push({
-        name,
-        reply
-    })
-    fs.writeFileSync(filePath, JSON.stringify(app_data))
+    client.query('INSERT INTO comment(post_id,username,reply) VALUES ($1,$2,$3)', [id, name, reply],
+        (err) => {
+            if (err) {
+                console.log(err.stack)
+            }
+        })
     res.status(202).send('Reply success')
 })
 
 app.listen(PORT, () => {
     console.log(`running on port ${PORT}`)
 })
+
